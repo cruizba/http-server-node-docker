449c34fb7cf1e23452a8dfe03668e4a5e067d77d
cruizba <carlos.ruizbal@gmail.com>
2021-12-08 19:45:36 +0100

    Don't push any new image if no new versions appears in npm

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ set -eu -o pipefail
 export DOCKERHUB_USERNAME="${DOCKERHUB_USERNAME}"
 export DOCKERHUB_PASSWORD="${DOCKERHUB_PASSWORD}"
 
+function docker_tag_exists() {
+    TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERHUB_USERNAME}'", "password": "'${DOCKERHUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+    curl --silent -f --head -lL https://hub.docker.com/v2/repositories/$1/tags/$2/ > /dev/null
+}
+
 function getLatestVersion() {
     local package_name=${1}
     npm view "${package_name}" version 
@@ -11,6 +16,13 @@ function getLatestVersion() {
 
 VERSION=$(getLatestVersion "http-server")
 echo "$VERSION"
+
+
+
+if docker_tag_exists cruizba/http-server "${VERSION}"; then
+    echo "The version exists. No new docker image will be created"
+    exit 0
+fi
 
 docker build --build-arg VERSION="${VERSION}" -t cruizba/http-server:"${VERSION}" .
 docker tag cruizba/http-server:"${VERSION}" cruizba/http-server:latest
