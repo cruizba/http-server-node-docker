25d55ae5b369a6fc5fd7a6ad2ac6d969386589b5
cruizba <carlos.ruizbal@gmail.com>
2021-12-08 19:32:19 +0100

    Add pub key

--- prepare_ci.sh
+++ prepare_ci.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eu -o pipefail
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update -y && sudo apt install -y docker-ce
